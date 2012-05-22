554c86bc274e524517cdb4042b060ee3e9be02d9
Peter Chronz <peter.Chronz@gwdg.de>
2012-05-22 18:35:37 +0200

    created template for analyze, analyze preview half working

--- app/controllers/Preview.scala
+++ app/controllers/Preview.scala
@@ -9,7 +9,16 @@ import play.api.data.Forms._
 
 object Preview extends Controller {
 
-  def analyze() = TODO
+  def analyze() = Action { implicit request =>
+    val energySeries = (Date(), 1500.0) :: Nil
+    val proteinSeries = (Date(), 1500.0) :: Nil
+    val fatSeries = (Date(), 1500.0) :: Nil
+    val carbsSeries = (Date(), 1500.0) :: Nil
+    val weightSeries = (Date(), 1500.0) :: Nil
+    val startDate = Date()
+    val endDate = Date()
+    Ok(views.html.previewAnalyze(energySeries, proteinSeries, fatSeries, carbsSeries, startDate.day, startDate.month, startDate.year, endDate.day, endDate.month, endDate.year, weightSeries))
+  }
 
   def measure() = TODO
 
